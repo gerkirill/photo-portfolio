410aa835611fd05ff166f53785bf210d2bcdfa86
ger <ger@dima-notebook.(none)>
2013-04-16 18:44:08 +0300

    enabled error reporting

--- web/app_dev.php
+++ web/app_dev.php
@@ -2,6 +2,9 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
