54d83d3b936565460647b4d820077f991ae8eaed
ger <ger@dima-notebook.(none)>
2013-04-16 18:23:17 +0300

    always work in dev mode for now

--- web/app.php
+++ web/app.php
@@ -1,6 +1,9 @@
 <?php
+/*
 if (FALSE !== strpos($_SERVER['HTTP_HOST'], 'dev')) {
     include(__DIR__ . '/app_dev.php');
 } else {
     include(__DIR__ . '/app_prod.php');
 }
+*/
+include(__DIR__ . '/app_dev.php');
