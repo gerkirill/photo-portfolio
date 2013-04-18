ae1795fde01224196ab635d9548da3d829a1e3f4
Kirill <ger.kirill@gmail.com>
2013-04-18 19:39:06 +0300

    added deploy script

---
+++ web/up.php
@@ -0,0 +1,20 @@
+<?php
+ob_implicit_flush(true);
+set_time_limit(0);
+chdir(dirname(__DIR__));
+$commands = array(
+        'git pull',
+        './composer.phar install',
+        'app/console -n doctrine:schema:update --force',
+        'app/console -n cache:clear',
+        'app/console -n cache:clear --env=prod',
+        'app/console -n assetic:dump',
+        'app/console -n assets:install',
+        'app/console -n doctrine:migrations:migrate'
+);
+foreach($commands as $command) {
+        echo 'Execute: '.$command."\n\n";
+        echo `$command`;
+        echo "\n-------------------------------------------------------------\n";
+}
+echo "\n";
