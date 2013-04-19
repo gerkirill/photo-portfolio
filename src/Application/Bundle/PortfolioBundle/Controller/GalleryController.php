0f289cae327eeb2a116cfa59647451fbb7de2b88
Dmitriy <demetriscompany@gmail.com>
2013-04-19 13:49:01 +0300

    без коментариев)

--- src/Application/Bundle/PortfolioBundle/Controller/GalleryController.php
+++ src/Application/Bundle/PortfolioBundle/Controller/GalleryController.php
@@ -13,9 +13,14 @@ class GalleryController extends Controller
 
     /**
      * @Route("/")
+	 * @Template("ApplicationPortfolioBundle:Gallery:index.html.twig")
      */
     public function indexAction()
     {
-    	return new Response('Gallery index page');
+    	$repository = $this->getDoctrine()->getRepository('ApplicationPortfolioBundle:Navigation');
+		$menu = $repository->findAll();
+		
+		//return new Response('Gallery index page');
+		return array('menus' => $menu);
     }
 }
---
+++ src/Application/Bundle/PortfolioBundle/Entity/Navigation.php
@@ -0,0 +1,40 @@
+<?php
+namespace Application\PortfolioBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="navigations")
+ */
+class Navigation
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+	protected $id;
+	
+	/**
+     * @ORM\Column(type="string", length=255)
+     */
+	protected $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+	protected $parentId;
+	
+	public function getId() {
+		return $this->id;
+	}
+	
+	public function getName() {
+		return $this->name;
+	}
+	
+	public function getParentId() {
+		return $this->parentId;
+	}
+}
+?>
