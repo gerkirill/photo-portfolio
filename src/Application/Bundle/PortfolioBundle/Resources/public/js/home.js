b6a4a5b69ebcb0f2f45bff1b0172cce457b456eb
Dmitriy <demetriscompany@gmail.com>
2013-04-26 15:45:09 +0300

    правки по ротатору

--- src/Application/Bundle/PortfolioBundle/Resources/public/js/home.js
+++ src/Application/Bundle/PortfolioBundle/Resources/public/js/home.js
@@ -1,8 +1,10 @@
 function rotate() {	
 	var current = ($('.big-image-container img.show')? $('.big-image-container img.show') : $('.big-image-container img:first'));
-	var next = ((current.next().length) ? ((current.next().hasClass('show')) ? $('.big-image-container img:first') :current.next()) : $('.big-image-container img:first'));  
-	current.removeClass('show').fadeOut("slow");
-	next.addClass('show').fadeIn("slow");
+	current.fadeOut("slow", function(next){
+		var next = ((current.next().length) ? ((current.next().hasClass('show')) ? $('.big-image-container img:first') :current.next()) : $('.big-image-container img:first'));  
+		$(this).removeClass('show');
+		next.addClass('show').fadeIn("slow");
+	});
 }
 $(document).ready(function(){
 	setInterval('rotate()',5000);
