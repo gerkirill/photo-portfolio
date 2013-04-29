ccca8412019ab94757c2864940dc86b1b31c451b
Dmitriy <demetriscompany@gmail.com>
2013-04-29 17:57:12 +0300

    загрузчик как отдельный сервис
    как то так, вроде правильно... но что внутри пока не соображу как
    правильно все сделать

---
+++ src/Application/Bundle/PortfolioBundle/Service/uploader.php
@@ -0,0 +1,13 @@
+<?php
+namespace Application\Bundle\PortfolioBundle\Service;
+
+use JMS\DiExtraBundle\Annotation\Service;
+
+/**
+ * @Service("portfolio.uploader", public=true)
+ */
+class Uploader
+{
+	
+}
+?>
