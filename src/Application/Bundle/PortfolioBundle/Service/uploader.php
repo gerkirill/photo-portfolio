60dc946577ee54b2a0ac84e49947eee350ce160d
Dmitriy <demetriscompany@gmail.com>
2013-04-30 15:50:02 +0300

    загрузчик (начало)

--- src/Application/Bundle/PortfolioBundle/Service/uploader.php
+++ src/Application/Bundle/PortfolioBundle/Service/uploader.php
@@ -8,6 +8,26 @@ use JMS\DiExtraBundle\Annotation\Service;
  */
 class Uploader
 {
+	protected $targetDir = 'uploads';
+	protected $cleanupTargetDir = true; // Remove old files
+	protected $maxFileAge = 5*3600; // Temp file age in seconds
+	protected $time_limit = 5*60; // 5 minutes execution time
 	
+	protected $chunk;
+	protected $chunks;
+	protected $fileName;
+	
+	function __construct()
+	{
+		
+	}
+	
+	public function setSettings($settings)
+	{
+		$this->targetDir = $settings['targetDir'];
+		$this->cleanupTargetDir = $settings['cleanupTargetDir'];
+		$this->maxFileAge = $settings['maxFileAge'];
+		$this->time_limit = $settings['time_limit'];
+	}
 }
 ?>
