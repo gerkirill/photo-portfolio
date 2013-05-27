58abd9e8d2b2d5659d0e2b02b6e75f9b5e6dca67
Dmitriy <demetriscompany@gmail.com>
2013-05-27 13:49:13 +0300

    прикрутил fosUserBundle
    не знаю правда насколько все правильно, но вроде получилось

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Application\Bundle\PortfolioBundle\ApplicationPortfolioBundle(),
 			new Gregwar\ImageBundle\GregwarImageBundle(),
+			new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
